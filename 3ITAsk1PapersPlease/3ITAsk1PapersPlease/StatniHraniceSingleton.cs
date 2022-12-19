3535633eddf7f626522586c9e7e3eb5909e20190
Matav <verner.matej@gmail.com>
2022-12-19 12:16:56 +0100

    Jedináček StatniHraniceSingleton.cs

---
+++ 3ITAsk1PapersPlease/3ITAsk1PapersPlease/StatniHraniceSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ITAsk1PapersPlease
+{
+    internal class StatniHraniceSingleton
+    {
+        private static StatniHraniceSingleton _instance;
+        public static StatniHraniceSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new StatniHraniceSingleton();
+                return _instance;
+            }
+        }
+        private StatniHraniceSingleton()
+        {
+
+        }
+
+        public List<string> seznamZakazanychJmen = new List<string>();
+    }
+}
